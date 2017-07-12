0d1a3712f649e52afb4a0aeb5c587cb8267619c6
xiongjun <674249291@qq.com>
2017-07-12 21:58:40 +0800

    添加dev

--- dt-business/business-web/src/main/java/com/fight/dt/business/web/Application.java
+++ dt-business/business-web/src/main/java/com/fight/dt/business/web/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by tpx on 2017/7/10.
  */
+@Profile("dev")
 @SpringBootApplication(scanBasePackages="com.fight.dt.business")
 public class Application extends SpringBootServletInitializer {
 
