44f53d635c293f1c590aa5b1d48c9488a910ef5f
tupengxiong <511980432@qq.com>
2017-07-13 23:05:33 +0800

    spring-boot-data-redis

--- dt-business/business-web/src/main/java/com/fight/dt/business/web/Application.java
+++ dt-business/business-web/src/main/java/com/fight/dt/business/web/Application.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by tpx on 2017/7/10.
@@ -19,6 +23,14 @@ public class Application extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        //测试redis发布与订阅
+        StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+        CountDownLatch latch = ctx.getBean(CountDownLatch.class);
+
+        template.convertAndSend("chat", "Hello from Redis!");
+
+        latch.await();
+
     }
 }
---
+++ dt-business/business-web/src/main/java/com/fight/dt/business/web/conf/SessionConfig.java
@@ -0,0 +1,67 @@
+package com.fight.dt.business.web.conf;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import java.util.concurrent.CountDownLatch;
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60*60*3)
+public class SessionConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(SessionConfig.class);
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+                                            MessageListenerAdapter listenerAdapter) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    @Bean
+    Receiver receiver(CountDownLatch latch) {
+        return new Receiver(latch);
+    }
+
+    @Bean
+    CountDownLatch latch() {
+        return new CountDownLatch(1);
+    }
+
+    @Bean
+    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    class Receiver {
+        private CountDownLatch latch;
+
+        @Autowired
+        public Receiver(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        public void receiveMessage(String message) {
+            log.debug("Received <" + message + ">");
+            latch.countDown();
+        }
+    }
+}
